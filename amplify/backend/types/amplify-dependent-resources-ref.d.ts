e26998ac827eac152dfb255cf691da3505f69adf
Pranav Burugula <13784743+pranavburugula@users.noreply.github.com>
2023-10-15 15:43:05 -0700

    feat: Added Amplify auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "centappbba75ae3": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
